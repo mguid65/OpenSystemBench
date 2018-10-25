fa9f95d3b70662c181d6f453b1ea3501e112bb2d
Matthew Guidry <mguid2088@gmail.com>
2018-10-25 18:08:17 -0500

    Ubuntu installer

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt install libcurl4-openssl-dev qt5-default qt5-qmake libgl1-mesa-dev
+
+qmake -config release
+
+make
+
+sudo cp ./bin/OpenSystemBench /usr/bin/OpenSystemBench
+sudo cp ./icon/OSBLogo.svg /usr/share/icons/OSBLogo.svg
+sudo cp ./desktop/OpenSystemBench.desktop /usr/share/applications/OpenSystemBench.desktop
+
+echo 'done'
